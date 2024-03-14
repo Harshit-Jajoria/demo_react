8b2bca62e6dd8c17e808d475717847b7038e7b34
Harshit-Jajoria <122677014+Harshit-Jajoria@users.noreply.github.com>
2024-03-14 15:22:12 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count} github webhook
+          count is {count} github webhook1
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR

816e1b66603970a39a4af2a6cb3f19234a8df05e
Harshit-Jajoria <122677014+Harshit-Jajoria@users.noreply.github.com>
2024-04-05 00:17:26 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 3)}>
-          count is {count} github harsdtfyguhijok
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count} github harshit
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
